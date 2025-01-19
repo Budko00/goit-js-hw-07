bc8b9d98c672d4682de438ba325a263d344bff0e
Budko Anastasiia <gavrikslon@gmail.com>
2025-01-19 13:31:02 +0200

    4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
 
+const loginForm = document.querySelector('.login-form');
+
+const loginInputs = loginForm.elements;
+
+loginForm.addEventListener('submit', (e) => {
+
+e.preventDefault();
+
+
+const emptyFields = Array.from(loginInputs).filter((input) => input.value.trim() === '');
+
+if (emptyFields.length > 0) {
+    alert('All form fields must be filled in');
+    return;
+}
+
+const formData = {};
+Array.from(loginInputs).forEach((input) => {
+    formData[input.name] = input.value.trim();
+});
+
+console.log(formData);
+
+loginForm.reset();
+});
